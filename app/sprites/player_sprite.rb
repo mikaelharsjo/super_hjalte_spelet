0aab29b0ca5d4e79b6b6eac4467ace501a64c5d4
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-09-22 13:21:23 +0200

    changed player image

--- app/sprites/player_sprite.rb
+++ app/sprites/player_sprite.rb
@@ -1,5 +1,5 @@
 class PlayerSprite < Joybox::Physics::PhysicsSprite
-  
+
   def initialize(world)
     @world = world
     @player_body = @world.new_body(
@@ -13,34 +13,34 @@ class PlayerSprite < Joybox::Physics::PhysicsSprite
         density: 1.0
       )
     end
-    super file_name: 'koalio_stand.png', body: @player_body
+    super file_name: 'hero.png', body: @player_body
     @alive = true
   end
-  
-  def alive?     
+
+  def alive?
     @alive && above_ground?
   end
-  
+
   def move_forward
     if alive?
       self.body.apply_force force:[5, 0], as_impulse: true
     end
   end
-  
+
   def jump
     if alive?
       self.body.apply_force force:[10, 40]
       SimpleAudioEngine.sharedEngine.playEffect 'jump.wav'
     end
   end
-  
+
   def die
     @alive = false
     self.run_action Blink.with times:50
     SimpleAudioEngine.sharedEngine.playEffect 'hurt.wav'
     SimpleAudioEngine.sharedEngine.pauseBackgroundMusic
   end
-  
+
   def above_ground?
     @player_body.position.y > 0
   end
