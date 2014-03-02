7c7fc7b45ad13948c7b076bc622d088e84a65759
Mikael Härsjö <mikaelharsjo@gmail.com>
2014-03-02 20:56:49 +0100

    Changed grunt sound when hurting enemy

--- app/sprites/enemy.rb
+++ app/sprites/enemy.rb
@@ -1,5 +1,6 @@
 class Enemy < Joybox::Physics::PhysicsSprite
 	def hurt
+		SimpleAudioEngine.sharedEngine.playEffect 'sounds/grunt.wav'
 		@lifes -= 1
 		if @lifes == 0
 			@world.destroy_body @body
@@ -11,9 +12,7 @@ class Enemy < Joybox::Physics::PhysicsSprite
 	def die
 		@alive = false
 		self.run_action Blink.with times:50
-		SimpleAudioEngine.sharedEngine.playEffect 'hurt.wav'
-		SimpleAudioEngine.sharedEngine.pauseBackgroundMusic
-		# changed
-		# notify_observers(self.body.position)
+		#SimpleAudioEngine.sharedEngine.playEffect 'sounds/grunt.wav'
+		#SimpleAudioEngine.sharedEngine.pauseBackgroundMusic
 	end
 end
